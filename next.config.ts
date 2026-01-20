9f7f98d5e804a561226e9a5fc303bdd1cb496ae0
nahomhailu12 <hailunahom47@gmail.com>
2026-01-20 19:12:12 +0300

    build error fixed

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["../prisma/generated"],
-  output: "standalone",
-  outputFileTracingIncludes: {
-    "/api/**/*": ["./prisma/generated/prisma/**/*"],
-    "/**/*": ["./prisma/generated/prisma/**/*"],
-  },
-  // ✅ Add this to prevent Next.js from trying to "browser-ify" Prisma
-  serverExternalPackages: ["@prisma/client", "ws"],
+  /* config options here */
 };
 
 export default nextConfig;
